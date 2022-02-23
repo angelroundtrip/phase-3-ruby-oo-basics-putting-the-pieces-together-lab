a2bfc263ed3261ffc454a363a96eae4918fddd01
Angel Rosario <arosario.us@gmail.com>
2022-02-23 17:10:31 -0500

    Complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+# require 'pry'
+class Book
 
+  attr_accessor :title, :author, :page_count, :genre
 
+  
+  def initialize(title="And Then There Were None")
+
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+
+  # instance method
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"  
+  end
+end
+
+# binding.pry
